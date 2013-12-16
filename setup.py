f4ff87c125c2c1e760b3d5fda2d37f5a1872d760
Richard Quirk <richard@quirk.es>
2013-12-16 10:01:28 +0100

    Fix setup.py
    Move all files to a module subdirectory and add the correct options to
    setup.py. This means things are now installable with:
    
        python setup.py install
    
    Instead of using uploadr.py directly, use the folders2flickr script.

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from distutils.core import setup
 
-setup(console=['uploadr.py'])
+setup(name='folders2flickr',
+      description="Uploads photos and videos to flickr.com",
+      url="https://github.com/richq/folders2flickr",
+      version="1.0",
+      scripts=['folders2flickr'],
+      license="GNU GPL v3",
+      packages=['f2flickr'],
+      author="Richard Quirk",
+      download_url="https://github.com/richq/folders2flickr",
+      author_email="richard.quirk@gmail.com",
+      install_requires=[''])
